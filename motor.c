0c0573dd91c66918b7fe824fc713bf2f1f5a9c53
luomo233 <157657883@qq.com>
2026-02-07 21:13:49 +0800

    refactor(motor): 重构电机驱动逻辑，使用TIM2 PWM直接控制
    - 移除旧的GPIO控制方式，改用TIM2的4个PWM通道直接驱动电机
    - 简化电机控制逻辑，去除方向配置参数
    - 优化PWM限制函数和延时函数
    - 更新刹车和转向功能的实现方式

--- motor.c
+++ motor.c
@@ -1,172 +1,175 @@
 #include "motor.h"
+#include "stm32f10x.h"
+#include "stm32f10x_tim.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
-#include "pwm.h"
 #include "delay.h"
 
-static uint8_t motor_direction_config = MOTOR_DIRECTION_FORWARD;
+/* * 硬件连接 (基于提供的原理图):
+ * 左电机 (A): PA0 (TIM2_CH1), PA1 (TIM2_CH2)
+ * 右电机 (B): PA2 (TIM2_CH3), PA3 (TIM2_CH4)
+ * 驱动器: DRV8833 (不需要独立Enable脚，直接PWM驱动方向脚)
+ */
 
-static void Motor_GPIO_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
-
-    GPIO_InitStruct.GPIO_Pin = IN1_PIN | IN2_PIN | IN3_PIN | IN4_PIN;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(MOTOR_PORT, &GPIO_InitStruct);
-
-    // 注释掉下面的部分
-    //  GPIO_InitStruct.GPIO_Pin = MOTOR_TT1_A | MOTOR_TT1_B;
-    //  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
-    //  GPIO_Init(MOTOR_TT1_PORT, &GPIO_InitStruct);
-
-    // GPIO_InitStruct.GPIO_Pin = MOTOR_TT2_A | MOTOR_TT2_B;
-    // GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
-    // GPIO_Init(MOTOR_TT2_PORT, &GPIO_InitStruct);
-
-    GPIO_ResetBits(MOTOR_PORT, IN1_PIN | IN2_PIN | IN3_PIN | IN4_PIN);
-}
-
-void Motor_Init(void)
-{
-    Motor_GPIO_Init();
-    PWM_Init();
-}
-
-void Motor_SetDirection(uint8_t direction)
-{
-    motor_direction_config = direction;
-}
-
-uint8_t Motor_GetDirection(void)
-{
-    return motor_direction_config;
-}
-
+// 限制PWM范围在 0-100
 static float limit_pwm(float pwm)
 {
-    return (pwm > 99) ? 99 : (pwm < 0) ? 0
-                                       : pwm;
+    if (pwm > 100)
+        return 100;
+    if (pwm < 0)
+        return 0;
+    return pwm;
 }
 
+// 延时辅助函数
 static void move_delay(float cm, float speed)
 {
+    // 简单的估算：假设速度系数，具体需根据实际跑动距离校准
     float time_ms = (cm * 60) / (speed / 10);
     if (speed <= 0)
         time_ms = 0;
     Delay_ms((uint16_t)time_ms);
 }
 
-void Motor_Forward(float left_pwm, float right_pwm)
+void Motor_Init(void)
 {
-    if (motor_direction_config == MOTOR_DIRECTION_FORWARD)
-    {
-        GPIO_SetBits(MOTOR_PORT, IN1_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN2_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN3_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN4_PIN);
-    }
-    else
-    {
-        GPIO_ResetBits(MOTOR_PORT, IN1_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN2_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN3_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN4_PIN);
-    }
+    GPIO_InitTypeDef GPIO_InitStructure;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_OCInitTypeDef TIM_OCInitStructure;
 
-    left_pwm = limit_pwm(left_pwm);
-    right_pwm = limit_pwm(right_pwm);
+    // 1. 开启 TIM2 和 GPIOA 时钟
+    // 注意：TIM2 是 APB1 总线
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-    PWM_SetCompare1((uint16_t)left_pwm);
-    PWM_SetCompare3((uint16_t)right_pwm);
+    // 2. 配置 PA0, PA1, PA2, PA3 为复用推挽输出 (AF_PP)
+    // 这些引脚将输出 PWM 波形
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // 3. 配置 TIM2 时基
+    // 频率 = 72MHz / (71+1) / (99+1) = 10kHz
+    TIM_TimeBaseStructure.TIM_Period = 99;    // ARR
+    TIM_TimeBaseStructure.TIM_Prescaler = 71; // PSC
+    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+    // 4. 配置 TIM2 的 4 个通道为 PWM模式
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 0; // 初始占空比为0
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+    TIM_OC1Init(TIM2, &TIM_OCInitStructure); // PA0
+    TIM_OC2Init(TIM2, &TIM_OCInitStructure); // PA1
+    TIM_OC3Init(TIM2, &TIM_OCInitStructure); // PA2
+    TIM_OC4Init(TIM2, &TIM_OCInitStructure); // PA3
+
+    // 使能预装载寄存器
+    TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+    // 5. 开启定时器
+    TIM_Cmd(TIM2, ENABLE);
 }
 
-void Motor_Back(float left_pwm, float right_pwm)
-{
-    if (motor_direction_config == MOTOR_DIRECTION_FORWARD)
-    {
-        GPIO_ResetBits(MOTOR_PORT, IN1_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN2_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN3_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN4_PIN);
-    }
-    else
-    {
-        GPIO_SetBits(MOTOR_PORT, IN1_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN2_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN3_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN4_PIN);
-    }
-
-    left_pwm = limit_pwm(left_pwm);
-    right_pwm = limit_pwm(right_pwm);
-
-    PWM_SetCompare1((uint16_t)left_pwm);
-    PWM_SetCompare3((uint16_t)right_pwm);
-}
-
-void Motor_Left(float right_pwm)
-{
-    GPIO_ResetBits(MOTOR_PORT, IN1_PIN);
-    GPIO_ResetBits(MOTOR_PORT, IN2_PIN);
-    PWM_SetCompare1(0);
-
-    if (motor_direction_config == MOTOR_DIRECTION_FORWARD)
-    {
-        GPIO_ResetBits(MOTOR_PORT, IN3_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN4_PIN);
-    }
-    else
-    {
-        GPIO_SetBits(MOTOR_PORT, IN3_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN4_PIN);
-    }
-
-    right_pwm = limit_pwm(right_pwm);
-    PWM_SetCompare3((uint16_t)right_pwm);
-}
-
-void Motor_Right(float left_pwm)
-{
-    GPIO_ResetBits(MOTOR_PORT, IN3_PIN);
-    GPIO_ResetBits(MOTOR_PORT, IN4_PIN);
-    PWM_SetCompare3(0);
-
-    if (motor_direction_config == MOTOR_DIRECTION_FORWARD)
-    {
-        GPIO_SetBits(MOTOR_PORT, IN1_PIN);
-        GPIO_ResetBits(MOTOR_PORT, IN2_PIN);
-    }
-    else
-    {
-        GPIO_ResetBits(MOTOR_PORT, IN1_PIN);
-        GPIO_SetBits(MOTOR_PORT, IN2_PIN);
-    }
-
-    left_pwm = limit_pwm(left_pwm);
-    PWM_SetCompare1((uint16_t)left_pwm);
-}
-
+// 停止所有电机
 void Motor_Stop(void)
 {
-    GPIO_ResetBits(MOTOR_PORT, IN1_PIN | IN2_PIN | IN3_PIN | IN4_PIN);
-    PWM_SetCompare1(0);
-    PWM_SetCompare3(0);
+    TIM_SetCompare1(TIM2, 0); // PA0 = 0
+    TIM_SetCompare2(TIM2, 0); // PA1 = 0
+    TIM_SetCompare3(TIM2, 0); // PA2 = 0
+    TIM_SetCompare4(TIM2, 0); // PA3 = 0
 }
 
+// 前进: IN1/IN3 输出 PWM, IN2/IN4 保持低电平
+void Motor_Forward(float left_pwm, float right_pwm)
+{
+    left_pwm = limit_pwm(left_pwm);
+    right_pwm = limit_pwm(right_pwm);
+
+    // 左轮前进 (PA0 PWM, PA1 Low)
+    TIM_SetCompare1(TIM2, (uint16_t)left_pwm);
+    TIM_SetCompare2(TIM2, 0);
+
+    // 右轮前进 (PA2 PWM, PA3 Low)
+    TIM_SetCompare3(TIM2, (uint16_t)right_pwm);
+    TIM_SetCompare4(TIM2, 0);
+}
+
+// 后退: IN2/IN4 输出 PWM, IN1/IN3 保持低电平
+void Motor_Back(float left_pwm, float right_pwm)
+{
+    left_pwm = limit_pwm(left_pwm);
+    right_pwm = limit_pwm(right_pwm);
+
+    // 左轮后退 (PA0 Low, PA1 PWM)
+    TIM_SetCompare1(TIM2, 0);
+    TIM_SetCompare2(TIM2, (uint16_t)left_pwm);
+
+    // 右轮后退 (PA2 Low, PA3 PWM)
+    TIM_SetCompare3(TIM2, 0);
+    TIM_SetCompare4(TIM2, (uint16_t)right_pwm);
+}
+
+// 左转: 左轮不动(或后退)，右轮前进
+// 这里实现为原地旋转（左轮后退，右轮前进），或者单轮转动，取决于你的习惯
+// 下面代码实现的是：左轮停止，右轮前进
+void Motor_Left(float right_pwm)
+{
+    right_pwm = limit_pwm(right_pwm);
+
+    // 左轮停止
+    TIM_SetCompare1(TIM2, 0);
+    TIM_SetCompare2(TIM2, 0);
+
+    // 右轮前进
+    TIM_SetCompare3(TIM2, (uint16_t)right_pwm);
+    TIM_SetCompare4(TIM2, 0);
+}
+
+// 右转: 左轮前进，右轮停止
+void Motor_Right(float left_pwm)
+{
+    left_pwm = limit_pwm(left_pwm);
+
+    // 左轮前进
+    TIM_SetCompare1(TIM2, (uint16_t)left_pwm);
+    TIM_SetCompare2(TIM2, 0);
+
+    // 右轮停止
+    TIM_SetCompare3(TIM2, 0);
+    TIM_SetCompare4(TIM2, 0);
+}
+
+// 左刹车
 void Motor_Left_Brake(void)
 {
-    GPIO_ResetBits(MOTOR_PORT, IN1_PIN);
-    GPIO_ResetBits(MOTOR_PORT, IN2_PIN);
-    PWM_SetCompare1(0);
+    TIM_SetCompare1(TIM2, 100); // DRV8833 两个脚都高电平是急刹，都低电平是滑行
+    TIM_SetCompare2(TIM2, 100); // 这里为了简单，设为0滑行停止，或者设为100急刹
+    // 为了保险起见，先用滑行停止
+    TIM_SetCompare1(TIM2, 0);
+    TIM_SetCompare2(TIM2, 0);
 }
 
+// 右刹车
 void Motor_Right_Brake(void)
 {
-    GPIO_ResetBits(MOTOR_PORT, IN3_PIN);
-    GPIO_ResetBits(MOTOR_PORT, IN4_PIN);
-    PWM_SetCompare3(0);
+    TIM_SetCompare3(TIM2, 0);
+    TIM_SetCompare4(TIM2, 0);
+}
+
+// 封装的高级功能
+void Motor_MoveForward(float cm, float left_speed, float right_speed)
+{
+    Motor_Forward(left_speed, right_speed);
+    move_delay(cm, (left_speed + right_speed) / 2);
+    Motor_Stop();
 }
 
 void Motor_MoveBack(float cm)
@@ -176,26 +179,27 @@ void Motor_MoveBack(float cm)
     Motor_Stop();
 }
 
-void Motor_MoveForward(float cm, float left_speed, float right_speed)
-{
-    Motor_Forward(left_speed, right_speed);
-    move_delay(cm, (left_speed + right_speed) / 2);
-    Motor_Stop();
-}
-
 void Motor_TurnRight90(void)
 {
-    Motor_Right_Brake();
-    Motor_Right(TURN_SPEED);
-    Delay_ms(800);
+    // 原地右转：左轮前，右轮后
+    TIM_SetCompare1(TIM2, (uint16_t)TURN_SPEED); // 左轮前
+    TIM_SetCompare2(TIM2, 0);
+    TIM_SetCompare3(TIM2, 0);
+    TIM_SetCompare4(TIM2, (uint16_t)TURN_SPEED); // 右轮后
+
+    Delay_ms(400); // 90度的时间需要根据实际情况调整
     Motor_Stop();
 }
 
 void Motor_TurnLeft90(void)
 {
-    Motor_Left_Brake();
-    Motor_Left(TURN_SPEED);
-    Delay_ms(800);
+    // 原地左转：左轮后，右轮前
+    TIM_SetCompare1(TIM2, 0);
+    TIM_SetCompare2(TIM2, (uint16_t)TURN_SPEED); // 左轮后
+    TIM_SetCompare3(TIM2, (uint16_t)TURN_SPEED); // 右轮前
+    TIM_SetCompare4(TIM2, 0);
+
+    Delay_ms(400); // 90度的时间需要根据实际情况调整
     Motor_Stop();
 }
 
--- pwm.c
+++ pwm.c
@@ -7,49 +7,44 @@
 
 void PWM_Init(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-    TIM_OCInitTypeDef TIM_OCInitStructure;
+    // GPIO_InitTypeDef GPIO_InitStructure;
+    // TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    // TIM_OCInitTypeDef TIM_OCInitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4, ENABLE);
+    // // 开启 TIM2 和 GPIOA 时钟
+    // RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    // RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    // // 配置 PA0, PA1, PA2, PA3 为复用推挽输出
+    // GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+    // GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    // GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    // GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    // // 初始化 TIM2
+    // TIM_TimeBaseStructure.TIM_Period = 99; // PWM 频率配置
+    // TIM_TimeBaseStructure.TIM_Prescaler = 71;
+    // TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+    // TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    // TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    // // 初始化 4 个通道的 PWM
+    // TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    // TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    // TIM_OCInitStructure.TIM_Pulse = 0;
+    // TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 
-    TIM_TimeBaseStructure.TIM_Period = PWM_PERIOD - 1;
-    TIM_TimeBaseStructure.TIM_Prescaler = 71;
-    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    // TIM_OC1Init(TIM2, &TIM_OCInitStructure); // PA0
+    // TIM_OC2Init(TIM2, &TIM_OCInitStructure); // PA1
+    // TIM_OC3Init(TIM2, &TIM_OCInitStructure); // PA2
+    // TIM_OC4Init(TIM2, &TIM_OCInitStructure); // PA3
 
-    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+    // TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    // TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    // TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    // TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);
 
-    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = 0;
-    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-
-    TIM_OC1Init(TIM3, &TIM_OCInitStructure);
-    TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-    TIM_OC2Init(TIM3, &TIM_OCInitStructure);
-    TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-    TIM_OC1Init(TIM4, &TIM_OCInitStructure);
-    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-    TIM_OC2Init(TIM4, &TIM_OCInitStructure);
-    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-    TIM_Cmd(TIM3, ENABLE);
-    TIM_Cmd(TIM4, ENABLE);
+    // TIM_Cmd(TIM2, ENABLE);
 }
 
 void PWM_SetCompare1(uint16_t Compare)

f74f1bd5906558bb2e0a3c29814cc9e3c0b720f1
luomo233 <157657883@qq.com>
2026-02-08 09:34:12 +0800

    refactor(main): 重构主逻辑和超声波模块
    优化主程序结构，将避障和巡航逻辑分离
    重构超声波模块，增加超时处理和错误检测
    调整电机速度和红外传感器参数

--- Ultrasound.c
+++ Ultrasound.c
@@ -1,50 +1,92 @@
 #include "stm32f10x.h" // Device header
+#include "Ultrasound.h"
 #include "Delay.h"
+
+#define ULTRA_PORT GPIOB
+#define ULTRA_TRIG_PIN GPIO_Pin_0
+#define ULTRA_ECHO_PIN GPIO_Pin_1
+
+// 超时阈值，防止while死循环 (约20ms)
+#define MAX_TIMEOUT 20000
 
 uint16_t Cnt;
 uint16_t OverCnt;
-void Ultrasound_Init()
+
+void Ultrasound_Init(void)
 {
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // trig
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; // echo
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-    TIM_InternalClockConfig(TIM1);
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+
+    // 开启时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+    // TRIG (PB0) -> 推挽输出
+    GPIO_InitStructure.GPIO_Pin = ULTRA_TRIG_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(ULTRA_PORT, &GPIO_InitStructure);
+
+    // ECHO (PB1) -> 下拉输入 (防止悬空误触)
+    GPIO_InitStructure.GPIO_Pin = ULTRA_ECHO_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+    GPIO_Init(ULTRA_PORT, &GPIO_InitStructure);
+
+    // 定时器初始化 (1us计数一次)
+    TIM_TimeBaseInitStructure.TIM_Period = 59999;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 71; // 72MHz/72 = 1MHz
     TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInitStructure.TIM_Period = 59999; // ARR
-    TIM_TimeBaseInitStructure.TIM_Prescaler = 71; // PSC
-    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
     TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
+
+    // 默认拉低TRIG
+    GPIO_ResetBits(ULTRA_PORT, ULTRA_TRIG_PIN);
 }
-float Test_Distance()
+
+float Test_Distance(void)
 {
-    GPIO_SetBits(GPIOB, GPIO_Pin_0);
+    uint32_t timeout = 0;
+
+    // 1. 发送至少10us的高电平触发信号
+    GPIO_SetBits(ULTRA_PORT, ULTRA_TRIG_PIN);
     Delay_us(20);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_0);
-    while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == RESET)
+    GPIO_ResetBits(ULTRA_PORT, ULTRA_TRIG_PIN);
+
+    // 2. 等待 ECHO 变高 (开始发送波)
+    timeout = 0;
+    while (GPIO_ReadInputDataBit(ULTRA_PORT, ULTRA_ECHO_PIN) == RESET)
     {
-    };
+        timeout++;
+        if (timeout > MAX_TIMEOUT)
+            return -1.0; // 超时，传感器未连接或损坏
+        Delay_us(1);
+    }
+
+    // 3. 开始计时
+    TIM_SetCounter(TIM1, 0);
     TIM_Cmd(TIM1, ENABLE);
-    while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == SET)
+
+    // 4. 等待 ECHO 变低 (收到回波)
+    timeout = 0;
+    while (GPIO_ReadInputDataBit(ULTRA_PORT, ULTRA_ECHO_PIN) == SET)
     {
-    };
+        timeout++;
+        if (timeout > MAX_TIMEOUT) // 超时 (超出最大量程)
+        {
+            TIM_Cmd(TIM1, DISABLE);
+            return 999.0; // 返回一个大数值表示无穷远
+        }
+        Delay_us(1);
+    }
+
+    // 5. 停止计时并计算
     TIM_Cmd(TIM1, DISABLE);
-    Cnt = TIM_GetCounter(TIM1);
-    float distance = (Cnt * 1.0 / 10 * 0.34) / 2;
-    TIM1->CNT = 0;
-    Delay_ms(100);
+    uint16_t count = TIM_GetCounter(TIM1);
+
+    // 距离(cm) = 时间(us) * 340m/s / 2 / 10000
+    //         = count * 0.017
+    float distance = (float)count * 0.017f;
+
+    Delay_ms(10); // 采样间隔
     return distance;
 }
--- motor.h
+++ motor.h
@@ -10,10 +10,10 @@
 // - IN1 = IN2: ??
 
 #define MOTOR_PORT GPIOA
-#define IN1_PIN GPIO_Pin_0  // ???IN1 (PA0, TIM2_CH1)
-#define IN2_PIN GPIO_Pin_1  // ???IN2 (PA1, TIM2_CH2)
-#define IN3_PIN GPIO_Pin_2  // ???IN3 (PA2, TIM2_CH3)
-#define IN4_PIN GPIO_Pin_3  // ???IN4 (PA3, TIM2_CH4)
+#define IN1_PIN GPIO_Pin_0 // ???IN1 (PA0, TIM2_CH1)
+#define IN2_PIN GPIO_Pin_1 // ???IN2 (PA1, TIM2_CH2)
+#define IN3_PIN GPIO_Pin_2 // ???IN3 (PA2, TIM2_CH3)
+#define IN4_PIN GPIO_Pin_3 // ???IN4 (PA3, TIM2_CH4)
 
 // ?????(???)
 #define MOTOR_TT1_PORT GPIOA
@@ -25,25 +25,25 @@
 #define MOTOR_TT2_B GPIO_Pin_7
 
 // ????
-#define NORMAL_LEFT_SPEED 70.0f   // ???????
-#define NORMAL_RIGHT_SPEED 70.0f  // ???????(??????)
+#define NORMAL_LEFT_SPEED 84.0f  // ???????
+#define NORMAL_RIGHT_SPEED 79.5f // ???????(??????)
 
-#define TURN_SPEED 50.0f          // ????
-#define BACK_SPEED 50.0f          // ????
+#define TURN_SPEED 50.0f // ????
+#define BACK_SPEED 50.0f // ????
 
 // ?????????
 #define IR_PORT GPIOA
-#define RED1_PIN GPIO_Pin_4   // PA4
-#define RED2_PIN GPIO_Pin_5   // PA5
-#define RED3_PIN GPIO_Pin_8   // PA8
-#define RED4_PIN GPIO_Pin_9   // PA9
-#define RED5_PIN GPIO_Pin_11  // PA11
-#define RED6_PIN GPIO_Pin_12  // PA12
+#define RED1_PIN GPIO_Pin_4  // PA4
+#define RED2_PIN GPIO_Pin_5  // PA5
+#define RED3_PIN GPIO_Pin_8  // PA8
+#define RED4_PIN GPIO_Pin_9  // PA9
+#define RED5_PIN GPIO_Pin_11 // PA11
+#define RED6_PIN GPIO_Pin_12 // PA12
 
 // ???????
 #define ULTRASONIC_PORT GPIOB
-#define ECHO_PIN GPIO_Pin_1   // PB1
-#define TRIG_PIN GPIO_Pin_0   // PB0
+#define ECHO_PIN GPIO_Pin_1 // PB1
+#define TRIG_PIN GPIO_Pin_0 // PB0
 
 // ????
 void Motor_Init(void);
